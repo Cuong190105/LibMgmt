99d83babe8a2e043c379f3abd40b2a7b31b1ba32
Kandien <daohoa2050@gmail.com>
2024-11-19 09:30:00 +0700

    update 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module org.example.libmgmt {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
-    requires org.apache.pdfbox;
-    requires javafx.swing;
 
 
     opens org.example.libmgmt to javafx.fxml;
