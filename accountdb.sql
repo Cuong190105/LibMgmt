77840bdf2496bd8eaf27a51c01d5ebc12664c463
Kandien <daohoa2050@gmail.com>
2024-12-07 18:16:09 +0700

    accountdb and librarydb (Structure-only)

---
+++ accountdb.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2024 at 12:13 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `accountdb`
+--
+CREATE DATABASE IF NOT EXISTS `accountdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `accountdb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `Username` varchar(255) NOT NULL,
+  `HashedPassword` varchar(255) NOT NULL,
+  `UID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`Username`),
+  ADD KEY `UID` (`UID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `librarydb`.`user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- librarydb (7).sql
+++ librarydb (7).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2024 at 02:27 PM
+-- Generation Time: Dec 07, 2024 at 12:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,6 +35,7 @@ CREATE TABLE `borrow` (
   `UID` int(11) NOT NULL,
   `DocID` int(11) NOT NULL,
   `BorrowingDate` date DEFAULT NULL,
+  `DueDate` date DEFAULT NULL,
   `ReturnDate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -48,8 +49,9 @@ DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
   `UID` int(11) NOT NULL,
   `DocID` int(11) NOT NULL,
-  `CommentContent` text DEFAULT NULL,
-  `Rating` int(11) DEFAULT NULL
+  `Content` text DEFAULT NULL,
+  `Rating` int(11) DEFAULT NULL,
+  `Date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -61,19 +63,31 @@ CREATE TABLE `comment` (
 DROP TABLE IF EXISTS `document`;
 CREATE TABLE `document` (
   `DocID` int(11) NOT NULL,
-  `Name` varchar(255) DEFAULT NULL,
+  `Title` varchar(255) DEFAULT NULL,
   `Author` varchar(255) DEFAULT NULL,
   `Publisher` varchar(255) DEFAULT NULL,
+  `PublishYear` int(11) DEFAULT NULL,
   `Quantity` int(11) DEFAULT NULL,
   `Tags` varchar(255) DEFAULT NULL,
   `Visited` int(11) DEFAULT NULL,
-  `type` tinyint(1) DEFAULT 0,
+  `thesis` tinyint(1) DEFAULT 0,
   `ISBN` varchar(50) DEFAULT NULL,
   `Votes` int(11) DEFAULT NULL,
   `Score` double DEFAULT NULL,
   `Cover` mediumblob DEFAULT NULL,
-  `Content` mediumblob DEFAULT NULL,
-  `Description` varchar(9999) DEFAULT NULL
+  `Description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documentcontent`
+--
+
+DROP TABLE IF EXISTS `documentcontent`;
+CREATE TABLE `documentcontent` (
+  `docID` int(11) DEFAULT NULL,
+  `content` longblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -91,8 +105,8 @@ CREATE TABLE `user` (
   `Address` text DEFAULT NULL,
   `PhoneNumber` varchar(20) DEFAULT NULL,
   `Email` varchar(255) DEFAULT NULL,
-  `SocialSecurityNumber` varchar(50) DEFAULT NULL,
-  `UserType` varchar(50) DEFAULT NULL,
+  `SSN` varchar(50) DEFAULT NULL,
+  `isLibrarian` tinyint(1) DEFAULT NULL,
   `avatar` mediumblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -119,6 +133,12 @@ ALTER TABLE `comment`
 --
 ALTER TABLE `document`
   ADD PRIMARY KEY (`DocID`);
+
+--
+-- Indexes for table `documentcontent`
+--
+ALTER TABLE `documentcontent`
+  ADD KEY `docID` (`docID`) USING BTREE;
 
 --
 -- Indexes for table `user`
@@ -150,15 +170,21 @@ ALTER TABLE `user`
 -- Constraints for table `borrow`
 --
 ALTER TABLE `borrow`
-  ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`),
-  ADD CONSTRAINT `borrow_ibfk_2` FOREIGN KEY (`DocID`) REFERENCES `document` (`DocID`);
+  ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `borrow_ibfk_2` FOREIGN KEY (`DocID`) REFERENCES `document` (`DocID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`),
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`DocID`) REFERENCES `document` (`DocID`);
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`DocID`) REFERENCES `document` (`DocID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `documentcontent`
+--
+ALTER TABLE `documentcontent`
+  ADD CONSTRAINT `documentcontent_ibfk_1` FOREIGN KEY (`docID`) REFERENCES `document` (`DocID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
